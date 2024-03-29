513c95ebb626af5aa5a6546213256b83d7d15433
davidbrai <davidbrai@gmail.com>
2024-03-29 14:34:35 +0100

    basic version of upgradability

--- src/Nouns.sol
+++ src/Nouns.sol
@@ -1,28 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import { ERC20 } from './libs/ERC20.sol';
+import { ERC20PermitUpgradeable } from '@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol';
 import { IERC721 } from './libs/IERC721.sol';
+import { UUPSUpgradeable } from './libs/UUPSUpgradeable.sol';
 
-contract Nouns is ERC20 {
+contract Nouns is ERC20PermitUpgradeable, UUPSUpgradeable {
 
     uint256 public constant MULTIPLIER = 1_000_000 * 1e18;
 
     IERC721 public immutable nounsToken;
 
+    address public owner;
+
     constructor(address nounsToken_) {
         nounsToken = IERC721(nounsToken_);
     }
 
-    /// @dev Returns the name of the token.
-    function name() public pure virtual override returns (string memory) {
-        return "Nouns";
+    function initialize(address owner_) public initializer {
+        owner = owner_;
+        __ERC20_init("Nouns", "NOUNS");
+        __ERC20Permit_init("Nouns");
     }
 
-    /// @dev Returns the symbol of the token.
-    function symbol() public pure virtual override returns (string memory) {
-        return "NOUNS";
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert('OnlyOwner');
+        _;
     }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 
     /// @dev Returns the decimals places of the token.
     function decimals() public view virtual override returns (uint8) {
